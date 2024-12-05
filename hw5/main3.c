60b8120ffc7473c41f3ca3bcaf0bc69daa1fff8c
Yct1130 <111268186+Yct1130@users.noreply.github.com>
2024-12-05 19:02:34 +0800

    Create main3.c

---
+++ hw5/main3.c
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main()
+{
+    int k=1;
+    for (int x=1; k<=9 and x<=9;x++){
+    
+        printf("%d*%d=%d\t",x,1,x*1);
+        printf("%d*%d=%d\t",x,2,x*2);
+        printf("%d*%d=%d\t",x,3,x*3);
+        printf("%d*%d=%d\t",x,4,x*4);
+        printf("%d*%d=%d\t",x,5,x*5);
+        printf("%d*%d=%d\t",x,6,x*6);
+        printf("%d*%d=%d\t",x,7,x*7);
+        printf("%d*%d=%d\t",x,8,x*8);
+        printf("%d*%d=%d\t",x,9,x*9);
+        printf("\n");
+    
+    }
+
+    return 0;
+}
